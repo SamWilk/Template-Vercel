8cbc63c1503a3c39cbb6cb3cea318e5ef29a97b1
Sam Wilk <82825157+SamWilk@users.noreply.github.com>
2026-06-30 22:04:48 -0400

    Adding speed insight

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Sam Wilk Consulting | Websites for Local Businesses",
@@ -20,7 +21,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
